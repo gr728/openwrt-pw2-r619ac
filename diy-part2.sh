8d9b3565e24dd655ac58c6024507200fec795571
gr728 <62072234+gr728@users.noreply.github.com>
2021-12-30 11:18:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+cp -rf $GITHUB_WORKSPACE/qcom-ipq4019-r619ac.dtsi target/linux/ipq40xx/files/arch/arm/boot/dts
